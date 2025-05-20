259a7eb22ce22a30f760a1c0f6c2de10f1cf1de8
Brian Andhika Putra <135279979+brianandhikap@users.noreply.github.com>
2025-05-20 09:22:09 +0700

    user.go

--- internal/db/user.go
+++ internal/db/user.go
@@ -1,9 +1,17 @@
 package db
 
-import "fmt"
+import (
+    "database/sql"
+    "log"
+)
+
+type User struct {
+    TelegramID int64
+    Initial string
+}
 
 func AddUser(id int64, initial string) error {
-    _, err := DB.Exec("REPLACE INTO users (telegram_id, initial) VALUES (?, ?)", id, initial)
+    _, err := DB.Exec("INSERT INTO users (telegram_id, initial) VALUES (?, ?) ON DUPLICATE KEY UPDATE initial = VALUES(initial)", id, initial)
     return err
 }
 
@@ -12,8 +20,31 @@ func RemoveUser(id int64) error {
     return err
 }
 
-func GetUserByID(id int64) (string, error) {
+func GetAllUsers() ([]User, error) {
+    rows, err := DB.Query("SELECT telegram_id, initial FROM users")
+    if err != nil {
+        return nil, err
+    }
+    defer rows.Close()
+
+    var users []User
+    for rows.Next() {
+        var u User
+        if err := rows.Scan(&u.TelegramID, &u.Initial); err != nil {
+            log.Println("Error scanning user:", err)
+            continue
+        }
+        users = append(users, u)
+    }
+
+    return users, nil
+}
+
+func GetUserInitial(id int64) (string, error) {
     var initial string
     err := DB.QueryRow("SELECT initial FROM users WHERE telegram_id = ?", id).Scan(&initial)
+    if err == sql.ErrNoRows {
+        return "", nil
+    }
     return initial, err
 }
